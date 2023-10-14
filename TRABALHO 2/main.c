8acbe16ba12a4caa7a41636e5aa0808bb71bdce6
JoaoPeres <jperesbertoncelo@gmail.com>
2023-10-14 18:31:01 -0300

    trabalhos organizados

---
+++ TRABALHO 2/main.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+#define ORDEM 5;
+#define TRUE 1;
+#define FALSE 0;
+
+void createBTree(){
+
+}
+
+void searchBTree(){
+
+}
+
+void insertBTree(){
+
+}
+
+void printBTree(){
+
+}
+
+int main(int argc, char *argv[]){
+    if(strcmp(argv[1],"-c")){
+
+    }else if(strcmp(argv[1],"-e")){
+
+    }else if(strcmp(argv[1],"-c")){
+
+    }else{
+        printf("Argumentos incorretos!");
+    }
+}
